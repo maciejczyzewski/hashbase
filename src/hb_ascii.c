c9acb7c7f4667036bb6e4a9523768aa093629c10
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2014-07-23 19:58:03 +0200

    Add files.

--- src/hb_ascii.c
+++
@@ -1,8 +0,0 @@
-/*
- * ASCII                                An implementation of hashbase commands.
- *
- * Version:                                    @(#)ascii.c    0.0.1    09/07/14
- * Authors:             Maciej A. Czyzewski, <maciejanthonyczyzewski@gmail.com>
- *
- */
-
--- src/hb_util.c
+++
@@ -1,8 +0,0 @@
-/*
- * UTIL                                                     A utility functions.
- *
- * Version:                                     @(#)util.c    0.0.1    09/07/14
- * Authors:             Maciej A. Czyzewski, <maciejanthonyczyzewski@gmail.com>
- *
- */
-
